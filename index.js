e02d08d803cc39712b55949c113ef5f7fb5bc17c
ShadowwMaks <maksimix.steam@mail.ru>
2020-10-27 16:56:13 +0300

    Фикс переменных среды

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const express = require('express');
 const morgan = require("morgan");
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+function checkEnv(key) {
+  if (process.env[key]) return process.env[key];
+  console.error(`Please, set ${key} environment variable`);
+  process.exit(1);
+}
+
+const PORT = checkEnv(PORT);
+const HOST = checkEnv(HOST);
+const TARGET = checkEnv(HOST);
+
 const options = {
-  target: process.env.TARGET,
+  target: TARGET,
   changeOrigin: true,
-  pathRewrite: {
-    '^/api': '',
-  },
  }
 
 const proxy = createProxyMiddleware(options);
 
 const app = express();
-
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
 
 app.use(morgan('dev'));
 app.use('/api', proxy);
